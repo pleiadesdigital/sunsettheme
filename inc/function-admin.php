dd6d5f3dbae7516bee7397315edb033743f0ce1f
Ronaldo Ortiz <ronyortizop@yahoo.com>
2017-08-24 10:05:52 -0400

    Video 3 - create Admin SubPages

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -6,12 +6,24 @@
 
 function sunset_add_admin_page() {
 
-  add_menu_page('Sunset Theme Options', 'Sunset', 'manage_options', 'alecaddd-sunset', 'sunset_theme_create_page', 'dashicons-nametag', 26);
+  // Generate Sunset Admin Page
+  add_menu_page('Sunset Theme Options', 'Sunset', 'manage_options', 'alecaddd_sunset', 'sunset_theme_create_page', 'dashicons-nametag', 26);
 
-}
+  // Generate Sunset Admin Sub Pages
+  add_submenu_page('alecaddd_sunset', 'Sunset Theme Options', 'Settings', 'manage_options', 'alecaddd_sunset', 'sunset_theme_create_page');
+  add_submenu_page('alecaddd_sunset', 'Sunset CSS Options', 'Custom CSS', 'manage_options', 'alecaddd_sunset_css', 'sunset_theme_settings_page');
+
+
+} //sunset_add_admin_page()
 add_action('admin_menu', 'sunset_add_admin_page');
 
-
+// callback function for Generate Admin Page
 function sunset_theme_create_page() {
-  // generation of our admin pages
+  require_once(get_template_directory() . '/inc/templates/sunset-admin.php');
 }
+
+// callback function for Generate Admin Subpages
+function sunset_theme_settings_page() {
+  // generation of our admin sub pages
+}
+
