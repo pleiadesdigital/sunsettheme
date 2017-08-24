4a5373c86aea272c6862e0c91b9a6802118af5d0
Ronaldo Ortiz <ronyortizop@yahoo.com>
2017-08-24 11:07:23 -0400

    Video 4 - Create Custom Options with Settings API

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -13,9 +13,30 @@ function sunset_add_admin_page() {
   add_submenu_page('alecaddd_sunset', 'Sunset Theme Options', 'Settings', 'manage_options', 'alecaddd_sunset', 'sunset_theme_create_page');
   add_submenu_page('alecaddd_sunset', 'Sunset CSS Options', 'Custom CSS', 'manage_options', 'alecaddd_sunset_css', 'sunset_theme_settings_page');
 
+  // Activate custom settings
+  add_action('admin_init', 'sunset_custom_settings');
+
 
 } //sunset_add_admin_page()
 add_action('admin_menu', 'sunset_add_admin_page');
+
+// create custom settings / record on the database
+function sunset_custom_settings() {
+  register_setting('sunset-settings-group', 'first_name');
+  add_settings_section('sunset-sidebar-options', 'Sidebar Options', 'sunset_sidebar_options', 'alecaddd_sunset');
+  add_settings_field('sidebar-name', 'First Name', 'sunset_sidebar_name', 'alecaddd_sunset', 'sunset-sidebar-options');
+}
+
+// callback for add_settings_section
+function sunset_sidebar_options() {
+  echo 'Customize your sidebar information';
+}
+
+// callback add_settings_field
+function sunset_sidebar_name() {
+  $firstName = esc_attr(get_option('first_name'));
+  echo '<input type="text" name="first_name" value="' . $firstName . '" placeholder="First name">';
+}
 
 // callback function for Generate Admin Page
 function sunset_theme_create_page() {
--- inc/templates/sunset-admin.php
+++ inc/templates/sunset-admin.php
@@ -1,4 +1,7 @@
 <h1>Sunset Theme Options</h1>
-<h3 class="title">Manage Options</h3>
-<p>Customize the default WordPress Appearance Options</p>
-<?php bloginfo('description'); ?>
+<?php settings_errors(); ?>
+<form method="post" action="options.php">
+  <?php settings_fields('sunset-settings-group'); ?>
+  <?php do_settings_sections('alecaddd_sunset'); ?>
+  <?php submit_button( 'Guardar elementos'); ?>
+</form>
