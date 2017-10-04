6aec6fa38b87624825f0da2f6d13fbf692512a2e
Ronaldo Ortiz <ronyortizop@yahoo.com>
2017-10-03 22:59:10 -0400

    Video 30 - Aside Post Format

---
+++ template-parts/content-aside.php
@@ -0,0 +1,41 @@
+<?php /* Aside  Post Format */ ?>
+
+
+<article id="post-<?php the_ID(); ?>" <?php post_class('sunset-format-aside'); ?>>
+
+  <!-- CONTENT HEADER -->
+  <header class="entry-header content-header blog-header">
+    <div class="content-header-wrapper">
+
+        <div class="aside-image">
+          <?php if (sunset_get_attachment()) : ?>
+            <div class="aside-featured-image background-image" style="background-image: url(<?php echo sunset_get_attachment(); ?>)">
+            </div>
+          <?php endif; ?>
+        </div><!-- class="aside-image" -->
+
+        <div class="aside-content">
+          <div class="entry-meta">
+            <?php echo sunset_posted_meta(); ?>
+          </div>
+          <div class="entry-excerpt">
+            <?php the_content(); ?>
+          </div>
+        </div><!-- class="aside-content" -->
+
+    </div><!-- class="content-header-wrapper" -->
+  </header><!-- class="content-header blog-section" -->
+
+
+  <!-- CONTENT FOOTER -->
+  <footer class="entry-footer content-footer blog-footer">
+
+    <div class="content-footer-wrapper">
+      <div class="footer-phantom"></div>
+      <div class="footer-content">
+        <?php echo sunset_posted_footer(); ?>
+      </div>
+    </div><!-- class="content-footer blog-footer" -->
+  </footer><!-- class="entry-footer content-footer-wrapper" -->
+
+</article>
